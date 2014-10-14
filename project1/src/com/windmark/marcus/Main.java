6d7c9a21f366c8db34c3c85f9e48c97de629ba9d
Marcus Windmark <marcus.windmark@gmail.com>
2014-10-14 21:41:51 +0800

    Working byte reader

--- project1/src/com/windmark/marcus/Main.java
+++ project1/src/com/windmark/marcus/Main.java
@@ -3,37 +3,34 @@ package com.windmark.marcus;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+
 
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        File file = new File("/home/marcus/Skola/ntnu/dataCompression/project1/testdata/test1.raw");
-        FileInputStream fis = null;
+    public static void main(String args[]) throws IOException {
+        FileInputStream fileInputStream = null;
 
         try {
-            fis = new FileInputStream(file);
-
-            System.out.println("Total file size to read (in bytes) : "
-            + fis.available());
-
-            int content;
-
-            while ((content = fis.read()) != -1) {
-                // convert to char and display it
-                System.out.print((char) content);
+            int data;
+            fileInputStream = new FileInputStream("/home/marcus/Skola/ntnu/dataCompression/project1/testdata/test1.raw");
+            for (int i = 0; i < 5; i++) {
+                data = fileInputStream.read();
+                System.out.print(" " + data);
             }
-
-        } catch (IOException e) {
-                e.printStackTrace();
+            /*
+            while ((data = fileInputStream.read()) != -1) {
+                System.out.print(" " + data);
+            }
+            */
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } finally {
-            try {
-                if (fis != null)
-                    fis.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+            if (fileInputStream != null)
+                fileInputStream.close();
         }
     }
 }
