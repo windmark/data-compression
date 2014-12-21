da5cc4a0b431c6940215691603f34ea3ecea3f72
Marcus Windmark <marcus.windmark@gmail.com>
2014-12-22 02:02:07 +0800

    Draft of DCT algorithm

---
+++ project2/src/DCT.java
@@ -0,0 +1,168 @@
+import static java.lang.Math.*;
+
+
+
+public class DCT {
+    private double cosTable[][];
+    private double transposedCosTable[][];
+    private final int blockSize;
+
+
+    public DCT(int blockSize) {
+        if (!isPowerOfTwo(blockSize)) {
+            throw new IllegalArgumentException("Block size must be a power of 2");
+        }
+
+        this.blockSize = blockSize;
+        cosTable = new double[blockSize][blockSize];
+        transposedCosTable = new double[blockSize][blockSize];
+
+        initCosTables();
+    }
+
+
+    // According to the DCT algorithm at
+    // http://www.cmlab.csie.ntu.edu.tw/cml/dsp/training/coding/transform/dct.html
+    private void initCosTables() {
+        for(int k = 0; k < blockSize; k++){
+            for(int n = 0; n < blockSize; n++){
+                double arg = PI * k * (2.0 * n + 1) / (2 * blockSize);
+                cosTable[k][n] = cos(arg);
+                transposedCosTable[n][k] = cosTable[k][n];
+            }
+        }
+    }
+
+
+    private double[] getRow(double[][] block, int row){
+        double[] output = new double[blockSize];
+
+        for(int column = 0; column < blockSize; column++){
+            output[column] = block[row][column];
+        }
+        return output;
+    }
+
+
+    private void insertRow(double[][] block, double[] row, int rowNumber){
+        for(int column = 0; column < blockSize; column++){
+            block[rowNumber][column] = row[column];
+        }
+    }
+
+
+    private double[] getColumn(double[][] block, int column){
+        double[] output = new double[blockSize];
+
+        for(int row = 0; row < blockSize; row++){
+            output[row] = block[row][column];
+        }
+        return output;
+    }
+
+
+    private void insertColumn(double[][] block, double[] column, int columnNumber){
+        for(int row = 0; row < blockSize; row++){
+            block[row][columnNumber] = column[row];
+        }
+    }
+
+
+    public double[][] forwardDCT(double[][] inBlock){
+        if (inBlock.length != blockSize || inBlock[0].length != blockSize) {
+            throw new IllegalArgumentException("Input matrix must be block sized");
+        }
+
+        double[][] outBlock = new double[blockSize][blockSize];
+
+        // Transforms rows
+        for (int row = 0; row < blockSize; row++) {
+            double[] transformedRow = forwardDCT1D(getRow(inBlock, row));
+            insertRow(outBlock, transformedRow, row);
+        }
+
+        // then transforms columns
+        for (int column = 0; column < blockSize; column++) {
+            double[] transformedColumn = forwardDCT1D(getColumn(inBlock, column));
+            insertColumn(outBlock, transformedColumn, column);
+        }
+        return outBlock;
+    }
+
+
+    private double[] forwardDCT1D(double[] valueArray) {
+        double[] outRow = new double[blockSize];
+
+        for (int k = 0; k < blockSize; k++) {
+            double sum = 0;
+
+            for (int n = 0; n < blockSize; n++) {
+                double cosine = cosTable[k][n];
+                double product = valueArray[n] * cosine;
+                sum += product;
+            }
+
+            double alpha;
+            if (k == 0) {
+                alpha = 1.0 / sqrt(2);
+            } else {
+                alpha = 1;
+            }
+            outRow[k] = sum * alpha * sqrt(2.0 / blockSize);
+        }
+        return outRow;
+    }
+
+
+    public double[][] inverseDCT(double[][] inBlock) {
+        if (inBlock.length != blockSize || inBlock[0].length != blockSize) {
+            throw new IllegalArgumentException("Input matrix must be block sized");
+        }
+
+        double[][] outBlock = new double[blockSize][blockSize];
+
+        // Inverse transform rows
+        for (int row = 0; row < blockSize; row++) {
+            double[] transformedRow = inverseDCT1D(getRow(inBlock, row));
+            insertRow(outBlock, transformedRow, row);
+        }
+
+        // then columns
+        for (int column = 0; column < blockSize; column++) {
+            double[] transformedColumn = inverseDCT1D(getColumn(inBlock, column));
+            insertColumn(outBlock, transformedColumn, column);
+        }
+        return outBlock;
+    }
+
+
+    private double[] inverseDCT1D(double[] valueArray) {
+        double[] outRow = new double[blockSize];
+
+        for (int n = 0; n < blockSize; n++) {
+            double sum = 0.0;
+
+            for (int k = 0; k < blockSize; k++) {
+                double cosine = transposedCosTable[n][k];
+                double product = valueArray[k] * cosine;
+                double alpha;
+
+                if (k == 0) {
+                    alpha = 1.0 / sqrt(2);
+                } else {
+                    alpha = 1;
+                }
+                sum += alpha * product;
+            }
+            outRow[n] = sum * sqrt(2.0 / blockSize);
+        }
+        return outRow;
+    }
+
+
+    private boolean isPowerOfTwo(int n) {
+        return (n > 0) && ((n & (n - 1)) == 0);
+    }
+}
+
+
---
+++ project2/src/TransformImageEncode.java
@@ -0,0 +1,127 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class TransformImageEncode {
+    private static final int IMAGE_WIDTH = 512;
+    private static final int IMAGE_HEIGHT = 512;
+    private static final int BLOCK_SIZE = 8;
+
+
+
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.err.println("Usage: java TransformImageEncode input_file_name output_file_name huffman_table_output_file_name");
+            System.exit(1);
+            return;
+        }
+
+        final long startTime = System.currentTimeMillis();
+
+        File inputFile = new File(args[0]);
+        File outputFile = new File(args[1]);
+        File HTOutputFile = new File(args[2]);
+
+        BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(inputFile));
+        BitOutputStream outputStream = new BitOutputStream(new BufferedOutputStream(new FileOutputStream(outputFile)));
+        PrintWriter HTOutputStream = new PrintWriter(HTOutputFile);
+
+
+
+
+
+        //////// DO STUFF
+        DCT dctTransformation = new DCT(BLOCK_SIZE);
+
+        int[][] imageMatrix = imageToMatrix(inputStream);
+        ArrayList<double[][]> tileList = imageToMatrix(imageMatrix);
+        ArrayList<double[][]> dctTileList = new ArrayList<double[][]>(tileList.size());
+
+        for (double[][] tile : tileList) {
+            dctTileList.add(dctTransformation.forwardDCT(tile));
+        }
+
+
+        ArrayList<double[][]> inverseDctTileList = new ArrayList<double[][]>(dctTileList.size());
+        for (double[][] tile : dctTileList) {
+            inverseDctTileList.add(dctTransformation.inverseDCT(tile));
+        }
+        inverseDctTileList.get(0);
+
+
+        //////////A TEMPORARY CHECK TO SEE THE DIFFERENCE BETWEEN BEFORE AND AFTER DCT AND INVERSE DCT
+        double sum = 0;
+
+        for (int h = 0; h < tileList.size(); h++) {
+            double[][] a = tileList.get(h);
+            double[][] b = inverseDctTileList.get(h);
+
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    sum += (b[i][j] - a[i][j]);
+                }
+            }
+        }
+        if (sum > 1) {
+            System.err.println("INVERSE DCT ERROR LARGER THAN 1");
+        }
+        System.out.println("Inverse DCT error: " + sum);
+        ///////////////////////////////
+
+
+        outputStream.close();
+        inputStream.close();
+        HTOutputStream.close();
+
+        final long endTime = System.currentTimeMillis();
+        System.out.println("Encoding execution time: " + (endTime - startTime) + " ms" );
+    }
+
+
+
+
+    private static ArrayList<double[][]> imageToMatrix(int[][] imageMatrix) {
+        int width = imageMatrix.length;
+        int height = imageMatrix[0].length;
+
+        ArrayList<double[][]> tileList = new ArrayList<double[][]>();
+
+
+        for (int i = 0; i < height / BLOCK_SIZE; i++) {
+            for (int j = 0; j < width / BLOCK_SIZE; j++) {
+                int xpos = j * BLOCK_SIZE;
+                int ypos = i * BLOCK_SIZE;
+
+                // Doing this for structural purposes
+                double[][] tile = new double[BLOCK_SIZE][BLOCK_SIZE];
+
+                for (int a = 0; a < BLOCK_SIZE; a++) {
+                    for (int b = 0; b < BLOCK_SIZE; b++) {
+                        tile[a][b] = imageMatrix[xpos + a][ypos + b];
+                    }
+                }
+                tileList.add(tile);
+            }
+        }
+        return tileList;
+    }
+
+
+
+    private static int[][] imageToMatrix(BufferedInputStream in) throws IOException {
+        int [][] matrix = new int[IMAGE_WIDTH][IMAGE_HEIGHT];
+
+        for (int x = 0; x < IMAGE_WIDTH; x++) {
+            for (int y = 0; y < IMAGE_HEIGHT; y++) {
+                matrix[x][y] = in.read();
+            }
+        }
+        return matrix;
+    }
+
+
+
+
+
+
+
+}
